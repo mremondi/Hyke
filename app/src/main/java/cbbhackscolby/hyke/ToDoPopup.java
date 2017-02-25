9fbc1800e340b64725dfc83e9f614eaefb794ae0
mremondi <mremondi@colby.edu>
2017-02-25 09:14:03 -0500

    recommitting with google services

--- app/src/main/java/cbbhackscolby/hyke/ToDoPopup.java
+++ app/src/main/java/cbbhackscolby/hyke/ToDoPopup.java
@@ -1,19 +1,11 @@
 package cbbhackscolby.hyke;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
-import android.view.View;
-
-import cbbhackscolby.hyke.fragments.ToDoFragment;
-
-/**
- * Created by mremondi on 2/25/17.
- */
 
 public class ToDoPopup extends DialogFragment {
 
@@ -50,19 +42,5 @@ public class ToDoPopup extends DialogFragment {
                     }
                 });
         return builder.create();
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        // Verify that the host activity implements the callback interface
-        try {
-            // Instantiate the NoticeDialogListener so we can send events to the host
-            mListener = (ToDoPopupListener) activity;
-        } catch (ClassCastException e) {
-            // The activity doesn't implement the interface, throw exception
-            throw new ClassCastException(activity.toString()
-                    + " must implement NoticeDialogListener");
-        }
     }
 }
