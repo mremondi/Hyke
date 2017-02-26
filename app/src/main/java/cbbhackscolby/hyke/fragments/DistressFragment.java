243450935babd94bfc8c7b65b98a42683b8f47e2
anantkes19 <anantkes@colby.edu>
2017-02-26 02:49:18 -0500

    Distress Text

--- app/src/main/java/cbbhackscolby/hyke/fragments/DistressFragment.java
+++ app/src/main/java/cbbhackscolby/hyke/fragments/DistressFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -31,6 +32,8 @@ public class DistressFragment extends Fragment {
         final DatabaseReference ref = database.getReference("users").child(uid).child("distress");
         ref.setValue(dist);
         final View rootView = inflater.inflate(R.layout.fragment_distress, null, false);
+
+        final TextView tvDistress = (TextView) rootView.findViewById(R.id.tvDistress);
         ImageView ivDistressSignal = (ImageView) rootView.findViewById(R.id.ivDistressSignal);
         ivDistressSignal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,10 +41,12 @@ public class DistressFragment extends Fragment {
                 if(dist) {
                     ref.setValue(false);
                     dist = false;
+                    tvDistress.setText("All Clear");
                 }
                 else {
                     ref.setValue(true);
                     dist = true;
+                    tvDistress.setText("Group Notified");
                 }
             }
         });
